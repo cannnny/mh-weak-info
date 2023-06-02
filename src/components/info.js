3fd766471265fa534c51c8cc02bfb240b7d94b72
cany <couleurvide1008@gmail.com>
2023-06-02 15:40:40 +0900

    選択用コンポーネントの作成

---
+++ src/components/info.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function info(){
+    return (
+        <div className = "select">
+            <h2>モンスターをえらんでね</h2>
+            <select className = "selectMenu">
+                <option value = "リオレウス">リオレウス</option>
+                <option value = "リオレイア">リオレイア</option>
+                <option value = "マガイマガド">マガイマガド</option>
+            </select>
+        </div>
+    );
+}
+
+export default info;
