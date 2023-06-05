b234c14da50cac58c45a58ca34d9bf11aae617f8
cany <couleurvide1008@gmail.com>
2023-06-05 14:21:20 +0900

    一旦Resultのインポートをなくす

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './reset.css';
 import './App.css';
 import Select from './components/Select';
-import Result from './components/Result';
+// import Result from './components/Result';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        モンハン弱点教えてくれるくん
-      </header>
       <Select />
-      <Result />
+      {/* <Result /> */}
     </div>
   );
 }
