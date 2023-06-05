0d874f4cd735293f5bbceff861fc8828b38d5f22
cany <couleurvide1008@gmail.com>
2023-06-05 14:21:48 +0900

    プルダウンリストの作成

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import {useState} from 'react';
 
 const Select = () => {
+    const monstersData = [
+        {
+            name: 'リオレウス',
+            weakPoint: '龍'
+        },
+        {
+            name: 'リオレイア',
+            weakPoint: '龍'
+        },
+        {
+            name: 'マガイマガド',
+            weakPoint: '水'
+        },
+    ];
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value)
+    };
+
     return (
-        <div className = "select">
+        <div>
+            <h1 className="title">モンハン弱点教えてくれるくん</h1>
             <h2>モンスターをえらんでね</h2>
-            <select className = "selectMenu">
-                <option value = "リオレウス">リオレウス</option>
-                <option value = "リオレイア">リオレイア</option>
-                <option value = "マガイマガド">マガイマガド</option>
+            <select className = "selectMenu"
+                    value = {selectedOption}
+                    onChange = {handleOptionChange}
+            >
+                <option value = ''>--- 選択 ---</option>
+                {monstersData.map((monster,i)=>{
+                    return (
+                        <option key={i}
+                                value = {monster.name}>{monster.name}
+                        </option>
+                    )
+                })}
             </select>
         </div>
     );
