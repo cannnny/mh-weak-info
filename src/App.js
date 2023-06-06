7a581a1f0016acc8a5a04e3e9edaa7c29167aadd
cany <couleurvide1008@gmail.com>
2023-06-06 12:49:09 +0900

    cssの適用

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './reset.css';
-import './App.css';
-import {useState} from 'react';
+import './App.scss';
+import { useState } from 'react';
 
 function App() {
   // 選択肢を用意
   const monstersData = [
     {
-        name: 'リオレウス',
-        weakPoint: '龍'
+      name: 'リオレウス',
+      weakPoint: '龍'
     },
     {
-        name: 'リオレイア',
-        weakPoint: '雷'
+      name: 'リオレイア',
+      weakPoint: '雷'
     },
     {
-        name: 'マガイマガド',
-        weakPoint: '水'
+      name: 'マガイマガド',
+      weakPoint: '水'
     },
   ];
 
@@ -28,25 +28,25 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1 className = "title">モンハン弱点教えてくれるくん</h1>
-            <h2>モンスターをえらんでね</h2>
-            {/* プルダウンを表示 */}
-            <select className = "selectMenu"
-                    onChange = {handleOptionChange}        
-            >
-                {monstersData.map((monster, index)=>{
-                    return (
-                        <option key={index}
-                                value = {index}>{monster.name}
-                        </option>
-                    );
-                })}
-            </select>
+    <div className="app">
+      <h1 className="title">モンハン弱点教えてくれるくん</h1>
+      <h2>モンスターをえらんでね</h2>
+      {/* プルダウンを表示 */}
+      <select className="selectMenu"
+        onChange={handleOptionChange}
+      >
+        {monstersData.map((monster, index) => {
+          return (
+            <option key={index}
+              value={index}>{monster.name}
+            </option>
+          );
+        })}
+      </select>
 
-            {/* 選択されたものに紐づく弱点を表示 */}
-            <h2>弱点は…</h2>
-            <div className = "result">{monstersData[selectedIndex].weakPoint}</div>
+      {/* 選択されたものに紐づく弱点を表示 */}
+      <h2>弱点は…</h2>
+      <div className="result">{monstersData[selectedIndex].weakPoint}</div>
     </div>
   );
 }
